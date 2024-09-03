3485a053cdf66a63d0a42ae0f3ea6a4d64946cec
ParrotXray <a0983805812@gmail.com>
2024-09-03 22:29:13 +0800

    Add: Add the custom linker file

--- x86/source/page.h
+++ x86/source/page.h
@@ -3,11 +3,13 @@
 
 #define PG_SIZE             1024
 #define PG_4K               4096
-#define MAP_ADDR            0x80000000
+#define MAP_ADDR            (0x80000000)
 
-#define PDE_P			(1 << 0)
-#define PDE_W			(1 << 1)
-#define PDE_U			(1 << 2)
-#define PDE_PS			(1 << 7)
+#define PG_4K_Aligned       __attribute__((aligned(PG_4K)))
+
+#define PDE_P			    (1 << 0)
+#define PDE_W			    (1 << 1)
+#define PDE_U			    (1 << 2)
+#define PDE_PS			    (1 << 7)
 
 #endif // PAGE_H
